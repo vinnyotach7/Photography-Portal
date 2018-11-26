5b9b58eefff842f4f00755a03e3c50ef83929d40
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-26 17:31:54 +0300

    installed  flask

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringaschool/Documents/Photography-Project/virtual"
+set -gx VIRTUAL_ENV "/home/moringaschool/PYTHON/Photography-Portal/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
