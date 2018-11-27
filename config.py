6473749bb4e6820afadbf6508bfe665b31b4e4a0
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-27 12:28:23 +0300

    created  config file with  production,test and development config classes ..... in my config classes i set the mail server ,username and secret key set to environ...also created the app config_options

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1205@localhost/port_photo'
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'sylveromondipitched_perfect'
+    SENDER_EMAIL = 'sylveromondi@gmail.com'
+    
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    # DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+class TestConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1205@localhost/port_test'
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}    
