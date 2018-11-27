1526682e80368fbe90deef551dfa314a8e1cd8ec
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-27 14:35:39 +0300

    created register_login form

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from . import  db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
 
     __tablename__ = 'users'
 
@@ -31,4 +38,4 @@ class User(db.Model):
         return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
-        return f'User 
+        return f'User'
