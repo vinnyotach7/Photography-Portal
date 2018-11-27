860b18a36b18ea2cc94f38524b7af1a057065a68
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-27 10:42:06 +0300

    set up attribute  property to save ,set,access and verify the passwpords

--- app/models.py
+++ app/models.py
@@ -5,10 +5,30 @@ from flask import Flask
 class User(db.Model):
 
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer,  primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True, index =True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
+
+
+    def save_user(self, user):
+        db.session.add(user)
+        db.session.commit()
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User 
