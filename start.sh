f5a3b8010aed5252bc3ae37be9bcaea5e27bea8f
vinnyotach7 <vinnyotach7@gmail.com>
2018-11-28 14:41:50 +0300

    changes to the files

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
+# export SECRET_KEY="1205"
+# export MAIL_USERNAME="sylveromondi@gmail.com"
+# export MAIL_PASSWORD="stacy2018."
+export MAIL_USERNAME=vinnyotach7@gmail.com
+export MAIL_PASSWORD=kalonje2018
+export SECRET_KEY="vinny
 export SECRET_KEY="1205"
-export MAIL_USERNAME="sylveromondi@gmail.com"
-export MAIL_PASSWORD="stacy2018."
+# export MAIL_USERNAME="sylveromondi@gmail.com"
+# export MAIL_PASSWORD="stacy2018.""
 
 python3.6 manage.py runserver
