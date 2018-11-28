11989fdebba35aa5d28c2d936141fea327a9388d
NickM101 <nickmunene101@gmail.com>
2018-11-28 09:47:52 +0300

    Added gallery component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'gallery', component: GalleryComponent }
 ];
 
 @NgModule({
